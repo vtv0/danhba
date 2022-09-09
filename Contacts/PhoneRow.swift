bd94c940989365291c20f24d484a0eab72e22413
vtv <vbc14365@gmail.com>
2022-09-09 17:28:08 +0700

    lan 10 chua co DB

---
+++ Contacts/PhoneRow.swift
@@ -0,0 +1,31 @@
+//
+//  AssetMH3.swift
+//  Contacts
+//
+//  Created by Vuong The Vu on 24/08/2022.
+//
+
+import Foundation
+
+
+class PhoneRow : NSObject , NSCopying {
+    public var PhoneNumber : String
+    public var PhoneType : String
+    public var DisplayStatus : Bool = false
+
+
+    init(phoneNumber: String , phoneType: String, displayStatus: Bool) {
+        self.PhoneNumber = phoneNumber
+        self.PhoneType = phoneType
+        self.DisplayStatus = displayStatus
+    }
+
+    init(phoneNumber: String , phoneType: String) {
+        self.PhoneNumber = phoneNumber
+        self.PhoneType = phoneType
+    }
+
+    func copy(with zone : NSZone? = nil) -> Any {
+        return PhoneRow(phoneNumber: PhoneNumber, phoneType: PhoneType, displayStatus: DisplayStatus)
+    }
+}
